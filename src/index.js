cda6869a420c0387694dc595b4a1f615d1757d49
PAVEL KOROTEEV <pavelkoroteev@yandex-team.ru>
2022-06-11 22:08:31 +0300

    add homedir logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { i18n } from './services/i18n.js';
 import { say } from './services/say.js';
 import { sayReady } from './services/sayReady.js';
 import { listen } from './services/listen.js';
+import { homedir } from 'os';
 
 process.stdin.resume();
 
@@ -12,6 +13,7 @@ export const app = () => {
 
     say(i18n.welcome);
 
+    process.chdir(homedir());
     sayReady();
 };
 
