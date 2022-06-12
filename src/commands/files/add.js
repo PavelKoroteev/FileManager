fb819897938358802fd2d46a8a2d9a917f894083
PAVEL KOROTEEV <pavelkoroteev@yandex-team.ru>
2022-06-12 13:11:23 +0300

    transfer to streams API

--- src/commands/files/add.js
+++ src/commands/files/add.js
@@ -1,5 +1,9 @@
-import fs from 'fs/promises';
+import fs from 'fs';
+import { pipeline, } from 'stream/promises'
 
 export const add = async (new_file_name) => {
-    await fs.writeFile(new_file_name, '');
-}
+    await pipeline(
+        '',
+        fs.createWriteStream(new_file_name)
+    );
+};
--- src/commands/files/cat.js
+++ src/commands/files/cat.js
@@ -1,14 +1,10 @@
-import fs from 'fs/promises';
-// import fs from 'fs';
+import fs from 'fs';
 import { pipeline } from 'stream/promises';
 
 export const cat = async (path_to_file) => {
-    // await pipeline(
-    //     // TODO use BOM
-    //     fs.createReadStream(path_to_file, 'utf-8'),
-    //     process.stdout,
-    // );
-    // console.log('FINISH');
-    const file = await fs.readFile(path_to_file, 'utf-8');
-    console.log(file);
+    await pipeline(
+        // TODO use BOM
+        fs.createReadStream(path_to_file, 'utf-8'),
+        process.stdout,
+    );
 }
