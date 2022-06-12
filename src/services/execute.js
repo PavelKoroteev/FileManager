5417af21300545b9596319fd606d2cccf2e98f37
PAVEL KOROTEEV <pavelkoroteev@yandex-team.ru>
2022-06-12 12:46:11 +0300

    self review

--- src/services/execute.js
+++ src/services/execute.js
@@ -1,10 +1,10 @@
 import { i18n } from './i18n.js';
 
 export const execute = async (callback, args = [], context = null) => {
-    // try {
+    try {
         await callback.apply(context, args);
-    // } catch (e) {
-    //     console.log(i18n.commandError);
-    //     console.log(e);
-    // }
+    } catch (e) {
+        console.log(i18n.commandError);
+        console.log(e);
+    }
 }
